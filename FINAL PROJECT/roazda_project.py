fd5891fe5cbf66343daff73ba7357e8f51790b54
Rodrigo Azuero <roazda@umich.edu>
2018-12-05 10:30:45 -0500

    Adding in functionalities for bru-seq/chase

--- FINAL PROJECT/roazda_project.py
+++ FINAL PROJECT/roazda_project.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from statistics import stdev
 from beautifultable import BeautifulTable
 from collections import Counter
 
@@ -13,9 +14,6 @@ def main():
 
     #all necessary containers for comparison for cell line A
     fusionNameCellA = set()
-    junctionReadCountCellA = dict()
-    spanningFragCountCellA= dict()
-    spliceTypeCellA = dict()
     leftGeneCellA = dict()
     leftGeneBreakCellA = dict()
     rightGeneCellA = dict()
@@ -25,9 +23,6 @@ def main():
 
     #all necessary containers for comparison for cell line B
     fusionNameCellB = set()
-    junctionReadCountCellB = dict()
-    spanningFragCountCellB= dict()
-    spliceTypeCellB = dict()
     leftGeneCellB = dict()
     leftGeneBreakCellB = dict()
     rightGeneCellB = dict()
@@ -37,9 +32,6 @@ def main():
 
     #all necessary containers for comparison for cell line C if it exists
     fusionNameCellC = set()
-    junctionReadCountCellC = dict()
-    spanningFragCountCellC= dict()
-    spliceTypeCellC = dict()
     leftGeneCellC = dict()
     leftGeneBreakCellC = dict()
     rightGeneCellC = dict()
@@ -49,9 +41,6 @@ def main():
 
     #all necessary containers for comparison for cell line D if it exists
     fusionNameCellD = set()
-    junctionReadCountCellD = dict()
-    spanningFragCountCellD= dict()
-    spliceTypeCellD = dict()
     leftGeneCellD = dict()
     leftGeneBreakCellD = dict()
     rightGeneCellD = dict()
@@ -59,6 +48,7 @@ def main():
     annotsCellD = dict()
     leftAndRightGenesD = list()
 
+    totalGeneList = list()
 
     #read thru each file and append/extend/update each container as necessary
     if os.path.exists(fileNameCellA):
@@ -74,9 +64,6 @@ def main():
                 right_break = parts[7].strip().split(':')
                 #add each element into its respective dictionary
                 fusionNameCellA.add(parts[0])
-                junctionReadCountCellA.update({parts[0]: parts[1]})
-                spanningFragsCellA.update({parts[0]: parts[2]})
-                spliceTypeCellA.update({parts[0]: parts[3]})
                 leftGeneCellA.update({parts[0]: left_rightGene[0]})
                 leftGeneBreakCellA.update({parts[0]: left_break[1]})
                 rightGeneCellA.update({parts[0]: left_rightGene[1]})
@@ -98,9 +85,6 @@ def main():
                 right_break = parts[7].strip().split(':')
                 #add each element into its respective dictionary
                 fusionNameCellB.add(parts[0])
-                junctionReadCountCellB.update({parts[0]: parts[1]})
-                spanningFragsCellB.update({parts[0]: parts[2]})
-                spliceTypeCellB.update({parts[0]: parts[3]})
                 leftGeneCellB.update({parts[0]: left_rightGene[0]})
                 leftGeneBreakCellB.update({parts[0]: left_break[1]})
                 rightGeneCellB.update({parts[0]: left_rightGene[1]})
@@ -122,9 +106,6 @@ def main():
                     right_break = parts[7].strip().split(':')
                     #add each element into its respective dictionary
                     fusionNameCellC.add(parts[0])
-                    junctionReadCountCellC.update({parts[0]: parts[1]})
-                    spanningFragsCellC.update({parts[0]: parts[2]})
-                    spliceTypeCellC.update({parts[0]: parts[3]})
                     leftGeneCellC.update({parts[0]: left_rightGene[0]})
                     leftGeneBreakCellC.update({parts[0]: left_break[1]})
                     rightGeneCellC.update({parts[0]: left_rightGene[1]})
@@ -144,9 +125,6 @@ def main():
                     right_break = parts[7].strip().split(':')
                     #add each element into its respective dictionary
                     fusionNameCellD.add(parts[0])
-                    junctionReadCountCellD.update({parts[0]: parts[1]})
-                    spanningFragsCellD.update({parts[0]: parts[2]})
-                    spliceTypeCellD.update({parts[0]: parts[3]})
                     leftGeneCellD.update({parts[0]: left_rightGene[0]})
                     leftGeneBreakCellD.update({parts[0]: left_break[1]})
                     rightGeneCellD.update({parts[0]: left_rightGene[1]})
@@ -155,19 +133,18 @@ def main():
                     leftAndRightGenesD.extend((left_rightGene[0], left_rightGene[1]))
 
     #add to each set the fusions that are shared among the cell lines being compared
-        AtoBNameConnections = intersection(fusionNameCellA, fusionNameCellB)
-        AtoCNameConnections = intersection(fusionNameCellA, fusionNameCellC)
-        AtoDNameConnections = intersection(fusionNameCellA, fusionNameCellD)
-        BtoCNameConnections = intersection(fusionNameCellB, fusionNameCellC)
-        BtoDNameConnections = intersection(fusionNameCellB, fusionNameCellD)
-        CtoDNameConnections = intersection(fusionNameCellD, fusionNameCellD)
-        AtoBtoCNameConnections = intersection(fusionNameCellA, fusionNameCellB, fusionNameCellC)
-        AtoBtoDNameConnections = intersection(fusionNameCellA, fusionNameCellB, fusionNameCellD)
-        AtoCtoDNameConnections = intersection(fusionNameCellA, fusionNameCellC, fusionNameCellD)
-        BtoCtoDNameConnections = intersection(fusionNameCellB, fusionNameCellC, fusionNameCellD)
-        AtoBtoCtoDNameConnections = intersection(fusionNameCellA, fusionNameCellB, fusionNameCellC, fusionNameCellD)
-
-
+        totalGeneList = leftAndRightGenesA + leftAndRightGenesB + leftAndRightGenesC + leftAndRightGenesD
+        AtoBNameConnections = set.intersection(fusionNameCellA, fusionNameCellB)
+        AtoCNameConnections = set.intersection(fusionNameCellA, fusionNameCellC)
+        AtoDNameConnections = set.intersection(fusionNameCellA, fusionNameCellD)
+        BtoCNameConnections = set.intersection(fusionNameCellB, fusionNameCellC)
+        BtoDNameConnections = set.intersection(fusionNameCellB, fusionNameCellD)
+        CtoDNameConnections = set.intersection(fusionNameCellD, fusionNameCellD)
+        AtoBtoCNameConnections = set.intersection(fusionNameCellA, fusionNameCellB, fusionNameCellC)
+        AtoBtoDNameConnections = set.intersection(fusionNameCellA, fusionNameCellB, fusionNameCellD)
+        AtoCtoDNameConnections = set.intersection(fusionNameCellA, fusionNameCellC, fusionNameCellD)
+        BtoCtoDNameConnections = set.intersection(fusionNameCellB, fusionNameCellC, fusionNameCellD)
+        AtoBtoCtoDNameConnections = set.intersection(fusionNameCellA, fusionNameCellB, fusionNameCellC, fusionNameCellD)
 
     #create a Counter that will organize the genes into most frequent
     #the counter is a dictionary where each key is each gene and the value will be its frequency
@@ -175,12 +152,32 @@ def main():
         mostComGenesB = Counter(leftAndRightGenesB)
         mostComGenesC = Counter(leftAndRightGenesC)
         mostComGenesD = Counter(leftAndRightGenesD)
-        comGenesA_B = intersection(set(leftAndRightGenesA), set(leftAndRightGenesB))
-        comGenesA_C = intersection(set(leftAndRightGenesA), set(leftAndRightGenesC))
-        comGenesA_D = intersection(set(leftAndRightGenesA), set(leftAndRightGenesD))
-        comGenesB_C = intersection(set(leftAndRightGenesB), set(leftAndRightGenesC))
-        comGenesB_D = intersection(set(leftAndRightGenesB), set(leftAndRightGenesD))
-        comGenesC_D = intersection(set(leftAndRightGenesC), set(leftAndRightGenesD))
+        comGenesA_B = set.intersection(set(leftAndRightGenesA), set(leftAndRightGenesB))
+        comGenesA_C = set.intersection(set(leftAndRightGenesA), set(leftAndRightGenesC))
+        comGenesA_D = set.intersection(set(leftAndRightGenesA), set(leftAndRightGenesD))
+        comGenesB_C = set.intersection(set(leftAndRightGenesB), set(leftAndRightGenesC))
+        comGenesB_D = set.intersection(set(leftAndRightGenesB), set(leftAndRightGenesD))
+        comGenesC_D = set.intersection(set(leftAndRightGenesC), set(leftAndRightGenesD))
+        comGenesA_B_C = set.intersection(set(leftAndRightGenesA), set(leftAndRightGenesB), set(leftAndRightGenesC))
+        comGenesA_B_D =set.intersection(set(leftAndRightGenesA), set(leftAndRightGenesB), set(leftAndRightGenesD))
+        comGenesA_C_D = set.intersection(set(leftAndRightGenesA), set(leftAndRightGenesC), set(leftAndRightGenesD))
+        comGenesB_C_D = set.intersection(set(leftAndRightGenesB), set(leftAndRightGenesC), set(leftAndRightGenesD))
+        comGenesA_B_C_D = set.intersection(set(leftAndRightGenesB), set(leftAndRightGenesA), set(leftAndRightGenesC), set(leftAndRightGenesD))
+
+    #create table to show number of fusions shared
+        FusionNumberTable = BeautifulTable()
+        FusionNumberTable.column_headers("Connection", "# of Fusions Shared", "# of Genes Shared")
+        FusionNumberTable.append_row("A-B", len(AtoBNameConnections), len(comGenesA_B))
+        FusionNumberTable.append_row("A-C", len(AtoCNameConnections), len(comGenesA_C))
+        FusionNumberTable.append_row("A-D", len(AtoDNameConnections), len(comGenesA_D))
+        FusionNumberTable.append_row("B-C", len(BtoCNameConnections), len(comGenesB_C))
+        FusionNumberTable.append_row("B-D", len(BtoDNameConnections), len(comGenesB_D))
+        FusionNumberTable.append_row("C-D", len(CtoDNameConnections), len(comGenesC_D))
+        FusionNumberTable.append_row("A-B-C", len(AtoBtoCNameConnections), len(comGenesA_B_C))
+        FusionNumberTable.append_row("A-B-D", len(AtoBtoDNameConnections), len(comGenesA_B_D))
+        FusionNumberTable.append_row("A-C-D", len(AtoCtoDNameConnections), len(comGenesA_C_D))
+        FusionNumberTable.append_row("B-C-D", len(BtoCtoDNameConnections), len(comGenesB_C_D))
+        FusionNumberTable.append_row("A-B-C-D", len(AtoBtoCtoDNameConnections), len(comGenesA_B_C_D))
 
     #Create the tables for each gene fusion connection
     #beautifultable is a python extension that allows our data to be printed to the terminal in a lovely clear fashion
@@ -222,6 +219,102 @@ def main():
             leftBreakDiff = int(leftGeneBreakCellC[fusion]) - int(leftGeneBreakCellD[fusion])
             rightBreakDiff = int(rightGeneBreakCellC[fusion]) - int(rightGeneBreakCellD[fusion])
             FusionTable.append_row("C-D", fusion, leftGene, rightGene, leftBreakDiff, rightBreakDiff, annotsCellC[fusion])
+
+        #create table to describe connections of fusions in 3+ cell lines
+        FusionTableThreePlus = BeautifulTable()
+        FusionTableThreePlus.column_headers("Fusion", "Connection", "Left Gene", "Right Gene", "Left Break St Dev", "Right Break St Dev", "Annotation")
+        for fusion in AtoBtoCNameConnections:
+            leftGene = leftGeneCellA[fusion]
+            rightGene = rightGeneCellA[fusion]
+            leftBreakSample = [leftGeneBreakCellA[fusion], leftGeneBreakCellB[fusion], leftGeneBreakCellC[fusion]]
+            leftStDev = stdev(leftBreakSample)
+            rightBreakSample = [rightGeneBreakCellA[fusion], rightGeneBreakCellB[fusion], rightGeneBreakCellC[fusion]]
+            rightStDev = stdev(rightBreakSample)
+            FusionTableThreePlus.append_row(fusion, 'A-B-C', leftGene, rightGene, leftStDev, rightStDev, annotsCellA[fusion])
+        for fusion in AtoBtoDNameConnections:
+            leftGene = leftGeneCellA[fusion]
+            rightGene = rightGeneCellA[fusion]
+            leftBreakSample = [leftGeneBreakCellA[fusion], leftGeneBreakCellB[fusion], leftGeneBreakCellD[fusion]]
+            leftStDev = stdev(leftBreakSample)
+            rightBreakSample = [rightGeneBreakCellA[fusion], rightGeneBreakCellB[fusion], rightGeneBreakCellD[fusion]]
+            rightStDev = stdev(rightBreakSample)
+            FusionTableThreePlus.append_row(fusion, 'A-B-D', leftGene, rightGene, leftStDev, rightStDev, annotsCellA[fusion])
+        for fusion in AtoCtoDNameConnections:
+            leftGene = leftGeneCellA[fusion]
+            rightGene = rightGeneCellA[fusion]
+            leftBreakSample = [leftGeneBreakCellA[fusion], leftGeneBreakCellC[fusion], leftGeneBreakCellD[fusion]]
+            leftStDev = stdev(leftBreakSample)
+            rightBreakSample = [rightGeneBreakCellA[fusion], rightGeneBreakCellC[fusion], rightGeneBreakCellD[fusion]]
+            rightStDev = stdev(rightBreakSample)
+            FusionTableThreePlus.append_row(fusion, 'A-C-D', leftGene, rightGene, leftStDev, rightStDev, annotsCellA[fusion])
+        for fusion in BtoCtoDNameConnections:
+            leftGene = leftGeneCellB[fusion]
+            rightGene = rightGeneCellB[fusion]
+            leftBreakSample = [leftGeneBreakCellB[fusion], leftGeneBreakCellC[fusion], leftGeneBreakCellD[fusion]]
+            leftStDev = stdev(leftBreakSample)
+            rightBreakSample = [rightGeneBreakCellB[fusion], rightGeneBreakCellC[fusion], rightGeneBreakCellD[fusion]]
+            rightStDev = stdev(rightBreakSample)
+            FusionTableThreePlus.append_row(fusion, 'B-C-D', leftGene, rightGene, leftStDev, rightStDev, annotsCellA[fusion])
+        for fusion in AtoBtoCtoDNameConnections:
+            leftGene = leftGeneCellA[fusion]
+            rightGene = rightGeneCellA[fusion]
+            leftBreakSample = [leftGeneBreakCellA[fusion], leftGeneBreakCellC[fusion], leftGeneBreakCellD[fusion], leftGeneBreakCellD]
+            leftStDev = stdev(leftBreakSample)
+            rightBreakSample = [rightGeneBreakCellA[fusion], rightGeneBreakCellC[fusion], rightGeneBreakCellD[fusion], rightGeneBreakCellD]
+            rightStDev = stdev(rightBreakSample)
+            FusionTableThreePlus.append_row(fusion, 'A-B-C-D', leftGene, rightGene, leftStDev, rightStDev, annotsCellA[fusion])
+
+        #create table to describe the genes seen multiple times within cell lines
+        FusionTableGeneCommonalities = BeautifulTable()
+        FusionTableGeneCommonalities.column_headers("Common Gene", "Occurences in Cell Line A", "Occurences in Cell Line B", "Occurences in Cell Line C", "Occurences in Cell Line D")
+        for gene in totalGeneList:
+            a_occur = 0
+            b_occur = 0
+            c_occur = 0
+            d_occur = 0
+            if gene in mostComGenesA and mostComGenesA[gene] > 1:
+                a_occur = mostComGenesA[gene]
+            if gene in mostComGenesB and mostComGenesB[gene] > 1:
+                b_occur = mostComGenesB[gene]
+            if gene in mostComGenesC and mostComGenesC[gene] > 1:
+                c_occur = mostComGenesC[gene]
+            if gene in mostComGenesD and mostComGenesD[gene] > 1:
+                d_occur = mostComGenesD[gene]
+            if a_occur > 1 or b_occur > 1 or c_occur > 1 or d_occur >1:
+                FusionTableGeneCommonalities.append_row(gene, a_occur, b_occur, c_occur, d_occur)
+
+    #dealing with the bru-chase/seq data when only two folders are given on the command line
+    else:
+        totalGeneList = leftAndRightGenesA + leftAndRightGenesB
+        AtoBNameConnections = set.intersection(fusionNameCellA, fusionNameCellB)
+        mostComGenesA = Counter(leftAndRightGenesA)
+        mostComGenesB = Counter(leftAndRightGenesB)
+        comGenesA_B = set.intersection(set(leftAndRightGenesA), set(leftAndRightGenesB))
+        FusionNumberTable = BeautifulTable()
+        #make its own general conection table
+        FusionNumberTable.column_headers("Connection", "# of Fusions Shared", "# of Genes Shared")
+        FusionNumberTable.append_row("A-B", len(AtoBNameConnections), len(comGenesA_B))
+        #make its own connection tables
+        FusionTable = BeautifulTable()
+        FusionTable.column_headers("Fusion Name", "Left Gene", "Right Gene", "Difference in Left Gene Breakpoint", "Difference in Right Gene Breakpoint", "Annotation of Fusion")
+        for fusion in AtoBNameConnections:
+            leftGene = leftGeneCellA[fusion]
+            rightGene = rightGeneCellA[fusion]
+            leftBreakDiff = int(leftGeneBreakCellA[fusion]) - int(leftGeneBreakCellB[fusion])
+            rightBreakDiff = int(rightGeneBreakCellA[fusion]) - int(rightGeneBreakCellB[fusion])
+            FusionTable.append_row(fusion, leftGene, rightGene, leftBreakDiff, rightBreakDiff, annotsCellA[fusion])
+        #make a table showing commonalities in gene
+        FusionTableGeneCommonalities = BeautifulTable()
+        FusionTableGeneCommonalities.column_headers("Common Gene", "Occurences in Cell Line A", "Occurences in Cell Line B")
+        for gene in totalGeneList:
+            a_occur = 0
+            b_occur = 0
+            if gene in mostComGenesA and mostComGenesA[gene] > 1:
+                a_occur = mostComGenesA[gene]
+            if gene in mostComGenesB and mostComGenesB[gene] > 1:
+                b_occur = mostComGenesB[gene]
+            if a_occur > 1 or b_occur > 1 or c_occur > 1 or d_occur >1:
+                FusionTableGeneCommonalities.append_row(gene, a_occur, b_occur)
 
 
 
